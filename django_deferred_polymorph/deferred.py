b7561458aa5622dde5b16fe6f2c2b33624f28535
David Danier <david.danier@team23.de>
2012-04-10 14:14:44 +0200

    sync with private code

--- django_deferred_polymorph/deferred.py
+++ django_deferred_polymorph/deferred.py
@@ -1,36 +1,90 @@
-from django.db.models.query_utils import DeferredAttribute
-
 # this is based on Django's internal deferring mechanisms
 # the main change is, that all fields are loaded at once, so this
 # doesn't issue tons of database queries
+from django.db.models.query_utils import DeferredAttribute
 
+
+class DeferredManager(object):
+    def __init__(self, instance, model, deferred_attrs):
+        self.instance = instance
+        self.model = model
+        self.deferred_attrs = deferred_attrs
+        self.is_loaded = False
+    
+    def load(self):
+        if self.is_loaded:
+            return
+        self.is_loaded = True
+        # We use only() instead of values() here because we want the
+        # various data coersion methods (to_python(), etc.) to be called
+        # here.
+        deferred_fields = [f for f in self.model._meta.fields if f.attname in self.deferred_attrs]
+        query_fields = [f.name for f in deferred_fields]
+        obj = self.model.base_objects.filter(pk=self.instance.pk).only(*query_fields)\
+            .using(self.instance._state.db).get()
+        for f in deferred_fields:
+            self.setattr(f.attname, getattr(obj, f.attname))
+    
+    # TODO: Recursively support properties?
+    def getattr(self, attname):
+        if attname in self.model.__dict__:
+            prop = self.model.__dict__[attname]
+            if hasattr(prop, '__get__'):
+                return prop.__get__(self.instance)
+        return self.instance.__dict__[attname]
+    
+    # TODO: Recursively support properties?
+    def setattr(self, attname, value):
+        if attname in self.model.__dict__:
+            prop = self.model.__dict__[attname]
+            if hasattr(prop, '__set__'):
+                prop.__set__(self.instance, value)
+                return
+        self.instance.__dict__[attname] = value
+
+
+class DeferredManagerAccess(object):
+    def __init__(self, model, deferred_attrs):
+        self.model = model
+        self.deferred_attrs = deferred_attrs
+    
+    def __get__(self, instance, owner):
+        try:
+            return instance._deferred_manager_obj
+        except AttributeError:
+            instance._deferred_manager_obj = DeferredManager(instance, self.model, self.deferred_attrs)
+            return instance._deferred_manager_obj
+
+
+# We overwrite evry method of DeferredAttribute, but still need to extend it as
+# otherwise isinstance() calls will fail
 class LoadAllDeferredAttribute(DeferredAttribute):
-    def __init__(self, attr, model, attrs):
-        super(LoadAllDeferredAttribute, self).__init__(attr, model)
-        self.all_deferred_fields = attrs
+    def __init__(self, attr, model):
+        self.attr = attr
+        self.model = model
     
     def __get__(self, instance, owner):
         """
         Retrieves and caches the value from the datastore on the first lookup.
         Returns the cached value.
         """
-        from django.db.models.fields import FieldDoesNotExist
-
         assert instance is not None
-        cls = self.model_ref()
         data = instance.__dict__
-        if data.get(self.field_name, self) is self:
+        if not instance._deferred_manager.is_loaded:
             # We use only() instead of values() here because we want the
             # various data coersion methods (to_python(), etc.) to be called
             # here.
-            assert self.field_name in self.all_deferred_fields
-            deferred_fields = [f for f in instance._meta.fields if f.attname in self.all_deferred_fields]
-            query_fields = [f.name for f in deferred_fields]
-            obj = cls._base_manager.filter(pk=instance.pk).only(*query_fields)\
-                .using(instance._state.db).get()
-            for f in deferred_fields:
-                data[f.attname] = getattr(obj, f.attname)
-        return data[self.field_name]
+            assert self.attr in instance._deferred_manager.deferred_attrs
+            instance._deferred_manager.load()
+        return instance._deferred_manager.getattr(self.attr)
+    
+    def __set__(self, instance, value):
+        """
+        Deferred loading attributes can be set normally (which means there will
+        never be a database lookup involved.
+        """
+        instance._deferred_manager.setattr(self.attr, value)
+
 
 # This function is needed because data descriptors must be defined on a class
 # object, not an instance, to have any effect.
@@ -53,39 +107,55 @@ def deferred_class_factory(model, attrs):
     # ("a" added)
     name = "%s_aDeferred_%s" % (model.__name__, '_'.join(sorted(list(attrs))))
 
-    overrides = dict([(attr, LoadAllDeferredAttribute(attr, model, attrs))
+    overrides = dict([(attr, LoadAllDeferredAttribute(attr, model))
             for attr in attrs])
     overrides["Meta"] = Meta
     overrides["__module__"] = model.__module__
     overrides["_deferred"] = True
+    overrides["_deferred_manager"] = DeferredManagerAccess(model, attrs)
     return type(name, (model,), overrides)
 
 # The above function is also used to unpickle model instances with deferred
 # fields.
 deferred_class_factory.__safe_for_unpickling__ = True
 
+
+# TODO: Cache deferred models!
 def deferred_child_class_factory(instance, child_model):
     base_model = instance.__class__
     base_m2m_fields = [f.attname for f in base_model._meta._many_to_many()]
-    base_fields = [f.attname for f in base_model._meta.fields if f not in base_m2m_fields and not f.primary_key]
+    base_fields = [f.attname for f in base_model._meta.fields if f.attname not in base_m2m_fields and not f.primary_key]
     child_m2m_fields = [f.attname for f in child_model._meta._many_to_many()]
-    child_fields = [f.attname for f in child_model._meta.fields if f not in child_m2m_fields and not f.primary_key]
+    child_fields = [f.attname for f in child_model._meta.fields if f.attname not in child_m2m_fields and not f.primary_key]
     deferred_attrs = [f for f in child_fields if not f in base_fields]
     if not deferred_attrs:
         return child_model
     return deferred_class_factory(child_model, deferred_attrs)
 
+
 def deferred_child_obj_factory(instance, child_model):
+    # if we already got the needed child model instance, just return it
+    base_model = instance.__class__
+    if issubclass(base_model, child_model):
+        return instance
+    # create new (deferred) instance
     deferred_model = deferred_child_class_factory(instance, child_model)
     deferred_obj = deferred_model()
     # copy all values to new obj and
     # make sure every primary key is set to 'pk'
     # (this is needed to set FOO_ptr_id on child instances)
-    for f in deferred_obj._meta.fields:
+    #print base_model._meta.fields
+    for f in base_model._meta.fields:
         if f.primary_key:
+            # we set both "pk" and f.attname, as this way we get both
+            # "..._ptr_id" and id to the right value
             setattr(deferred_obj, f.attname, instance.pk)
-        elif f.attname in instance.__dict__:
-            deferred_obj.__dict__[f.attname] = instance.__dict__[f.attname]
+            setattr(deferred_obj, 'pk', instance.pk)
+        else:
+            setattr(deferred_obj, f.attname, getattr(instance, f.attname, None))
+        # old version, which does not encount for __get__/__set__ attributes
+        #elif f.attname in instance.__dict__:
+        #    deferred_obj.__dict__[f.attname] = instance.__dict__[f.attname]
     # clone db state
     # TODO: Clone more state?
     deferred_obj._state.db = instance._state.db
--- django_deferred_polymorph/models.py
+++ django_deferred_polymorph/models.py
@@ -2,16 +2,21 @@
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
 from django_deferred_polymorph.manager import DeferredPolymorphManager
+from django.db.models.signals import class_prepared
 
 
 class DeferredPolymorphBaseModel(models.Model):
-    _poly_ct = models.ForeignKey(ContentType, related_name='+')
+    _poly_ct = models.ForeignKey(ContentType, related_name='+', editable=False)
     
     objects = DeferredPolymorphManager()
+    base_objects = models.Manager()
     
-    def save(self, *args, **kwargs):
+    def _fill_poly_ct(self):
         if self._poly_ct_id is None:
             self._poly_ct = ContentType.objects.get_for_model(self.__class__)
+    
+    def save(self, *args, **kwargs):
+        self._fill_poly_ct()
         return super(DeferredPolymorphBaseModel, self).save(*args, **kwargs)
     
     def get_real_instance_class(self):
@@ -23,6 +28,12 @@ class DeferredPolymorphBaseModel(models.Model):
         if self.__class__ is model: # or (self._deferred and self.__class__.__bases__[0] is model):
             return self
         return model._default_manager.get(pk = self.pk)
+    
+    def delete(self, *args, **kwargs):
+        if self._deferred:
+            self.get_real_instance().delete(*args, **kwargs)
+        else:
+            super(DeferredPolymorphBaseModel, self).delete(*args, **kwargs)
     
     class Meta:
         abstract = True
@@ -42,4 +53,28 @@ class SubDeferredPolymorphBaseModel(DeferredPolymorphBaseModel):
     
     class Meta:
         abstract = True
+
+
+# Background: Django will use our DeferredPolymorphManager for reverse relations,
+# this includes the parent relation of the models (child.parent will result
+# in getting an Parent_aDeferred_... class. This is not intended, as we cannot
+# access real parents because of this. In addition it will break Django, when it
+# comes to deletion of objects (deferred objects just delete the child, again).
+# To fix this the solution django_polymorphic implemented is mimiced here. It
+# replaces the parent relation accessors, to use Parent.base_objects instead of
+# Parent._default_manager. This way we get our vanilla base model back.
+def fix_parent_and_child_relation(sender, **kwargs):
+    model = sender
+    if not issubclass(model, DeferredPolymorphBaseModel):
+        return
+    for parent, field in model._meta.parents.iteritems():
+        if field is None:
+            # not sure, what this means
+            # seems to happen for deferred classes (and subclasses?)
+            continue
+        if not issubclass(parent, DeferredPolymorphBaseModel):
+            continue
+        setattr(model, field.name, property(lambda self: parent.base_objects.get(pk=self.pk)))
+        setattr(parent, field.related.var_name, property(lambda self: model.base_objects.get(pk=self.pk)))
+class_prepared.connect(fix_parent_and_child_relation)
 
