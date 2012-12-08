7997b4fb2eb827ec91524af307b363f36474ef7c
David Danier <david.danier@team23.de>
2012-12-08 22:49:02 +0100

    Added setup.py and cleanup

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "django_deferred_polymorph",
+    version = "0.1.0",
+    description = 'Polymorphic models based on django deferred models',
+    author = 'David Danier',
+    author_email = 'david.danier@team23.de',
+    url = 'https://github.com/ddanier/django_deferred_polymorph',
+    long_description=open('README.rst', 'r').read(),
+    packages = [
+        'django_deferred_polymorph',
+    ],
+    requires = [
+        'django(>=1.4)',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Topic :: Utilities'
+    ],
+)
+
